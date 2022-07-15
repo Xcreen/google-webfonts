a9b962e5acdf5f84428130555139bd97ef3bfeb8
Xcreen <Xcreen@users.noreply.github.com>
2022-07-15 12:41:56 +0200

    Request Google Webfont-API and save response to disk

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const refreshService = require('./service/refreshService')
 
 const app = express()
 
@@ -8,6 +9,8 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.send('Initial Commit!')
 })
+
+refreshService.start()
 
 app.listen(process.env.EXPRESS_PORT, () => {
   console.log(`Starting Express on port: ${process.env.EXPRESS_PORT}`)
--- backend/src/service/refreshService.js
+++ backend/src/service/refreshService.js
@@ -1,22 +1,59 @@
-let serviceRunning = false
-let serviceIntervalTimeout = 15 * 60 * 1000
+const axios = require('axios').default
+const fs = require('fs')
 
-const refreshConfig = () => {
+let serviceRunning = false
+const serviceIntervalTimeout = 15 * 60 * 1000
+const googleWebFontAPIUrl = 'https://www.googleapis.com/webfonts/v1/webfonts'
+
+const refreshConfig = async () => {
   console.log('Refeshing Google Webfonts ...')
-  //TODO: Get current Google Webfonts JSON
+  try {
+    //Request Google Webfont-API
+    let googleWebFontResponse = null;
+    try {
+      googleWebFontResponse = await axios({
+        method: 'get',
+        url: googleWebFontAPIUrl,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        params: {
+          key: process.env.GOOGLE_WEBFONT_API_KEY,
+          sort: 'ALPHA'
+        }
+      })
+    } catch (requestException) {
+      let extraText = requestException.code
+      if (typeof requestException.response !== 'undefined' && typeof requestException.response.status !== 'undefined') {
+        extraText = `(${requestException.code} ${requestException.response.status})`
+      }
+      console.log(`Failed to request ${googleWebFontAPIUrl} ${extraText}`)
+      return
+    }
+    //Save Google-Webfont-Response to disk
+    let googleResponseData = JSON.stringify(googleWebFontResponse.data)
+    fs.writeFile(process.cwd() + '/cache/webfonts.json', googleResponseData, err => {
+      if (err) console.error(err)
+      console.log('Successfully refreshed Google Webfonts!')
+    })
+  }
+  catch (ex) {
+    console.log(ex)
+  }
 }
 
 module.exports = {
   start: () => {
     console.log('Starting Refresh-Service')
     serviceRunning = true
-    refreshConfig();
+    refreshConfig()
     let serviceInterval = setInterval(() => {
-      if(!serviceRunning) {
+      if (!serviceRunning) {
         clearInterval(serviceInterval)
         return
       }
-      refreshConfig();
+      refreshConfig()
     }, serviceIntervalTimeout)
   },
   stop: () => {
