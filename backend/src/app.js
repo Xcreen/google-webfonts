5ee0df4beabdb0d3fcf13f5fa8d9f2e1ade68486
Xcreen <Xcreen@users.noreply.github.com>
2022-07-15 12:48:04 +0200

    Added endpoint to stream google webfont response

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,21 @@
 require('dotenv').config()
 const express = require('express')
+const fs = require('fs')
 const refreshService = require('./service/refreshService')
 
-const app = express()
+refreshService.start()
 
+const app = express()
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('Initial Commit!')
 })
 
-refreshService.start()
+app.get('/google-webfont-api-data', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  fs.createReadStream(process.cwd() + '/cache/webfonts.json').pipe(res)
+})
 
 app.listen(process.env.EXPRESS_PORT, () => {
   console.log(`Starting Express on port: ${process.env.EXPRESS_PORT}`)
