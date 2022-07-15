62ea5fb6bc4fec85399e2ae54a7190d249ad7bed
Xcreen <Xcreen@users.noreply.github.com>
2022-07-15 15:47:56 +0200

    Added correct links

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,15 +1,15 @@
 <template>
-  <footer class="py-5">
+  <footer>
     <div class="d-flex justify-content-between py-4 my-4 border-top">
       <p>© 2022 Xcreen</p>
       <ul class="list-unstyled d-flex">
         <li class="ms-3">
-          <a class="link-dark" href="http://google.de" target="_blank" data-bs-toggle="tooltip" data-bs-placement="top" title="Xcreen on Github">
+          <a class="link-dark" href="https://github.com/Xcreen" target="_blank" data-bs-toggle="tooltip" data-bs-placement="top" title="Xcreen on Github">
             <i class="fa-brands fa-github fa-xl"></i>
           </a>
         </li>
         <li class="ms-3">
-          <a class="link-dark" href="#" target="_blank" data-bs-toggle="tooltip" data-bs-placement="top" title="Xcreen on Twitter">
+          <a class="link-dark" href="https://twitter.com/xWaschix" target="_blank" data-bs-toggle="tooltip" data-bs-placement="top" title="Xcreen on Twitter">
             <i class="fa-brands fa-twitter fa-xl"></i>
           </a>
         </li>
