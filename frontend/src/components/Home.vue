30829bb7349103c0f4210942022dcbe61f057aff
Xcreen <Xcreen@users.noreply.github.com>
2022-07-15 16:51:24 +0200

    Request/Display and search webfonts

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -6,17 +6,16 @@
       <div class="col-lg-6 mx-auto">
         <p class="lead mb-4 text-center">Easily integrate your favorite google webfonts locally into your website.</p>
         <div class="form-floating w-100 mb-4">
-          <input type="text" class="form-control" id="search-font-input" v-model="inputFontSearch" placeholder="Search fonts...">
+          <input type="text" class="form-control" id="search-font-input" v-model="inputFontSearch" @keyup="filterFonts" placeholder="Search fonts...">
           <label for="search-font-input">Search fonts...</label>
         </div>
 
         <span class="lead">Available fonts:</span>
 
         <ul class="list-group mt-2">
-          <a href="#" class="list-group-item list-group-item-action">The current link item</a>
-          <a href="#" class="list-group-item list-group-item-action">A second link item</a>
-          <a href="#" class="list-group-item list-group-item-action">A third link item</a>
-          <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
+          <a v-for="webfont in displayWebFonts" href="#" class="list-group-item list-group-item-action">
+            {{ webfont.family }} <small class="text-muted ms-2">{{ webfont.category }}</small>
+          </a>
         </ul>
 
       </div>
@@ -27,11 +26,57 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'home',
   data() {
     return {
-      inputFontSearch: ''
+      inputFontSearch: '',
+      googleWebFonts: [],
+      displayWebFonts: [],
+      searchInterval: null
+    }
+  },
+  methods: {
+    filterFonts() {
+      clearInterval(this.searchInterval)
+      this.searchInterval = setInterval(() => {
+        if(this.inputFontSearch.trim().length > 0) {
+          let foundedFonts = []
+          this.googleWebFonts.forEach(value => {
+            if(value.family.toLowerCase().includes(this.inputFontSearch.trim().toLowerCase())) {
+              foundedFonts.push(value)
+            }
+          })
+          this.displayWebFonts = foundedFonts
+        }
+        else {
+          this.displayWebFonts = this.googleWebFonts
+        }
+        clearInterval(this.searchInterval)
+      }, 500)
+    }
+  },
+  async mounted() {
+    try {
+      const googleWebFontResponse = await axios({
+        method: 'get',
+        url: this.$apiURL + 'google-webfont-api-data',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      })
+      this.googleWebFonts = googleWebFontResponse.data.items
+      this.displayWebFonts = this.googleWebFonts
+    }
+    catch (requestException) {
+      let extraText = requestException.code
+      if (typeof requestException.response !== 'undefined' && typeof requestException.response.status !== 'undefined') {
+        extraText = `(${requestException.code} ${requestException.response.status})`
+      }
+      console.log(`Failed to request ${this.$apiURL} ${extraText}`)
     }
   }
 }
