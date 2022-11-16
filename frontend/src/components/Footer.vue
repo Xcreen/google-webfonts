7e4e67ae33927d9827a2d11b3c2b4aeb170508c8
Xcreen <Xcreen@users.noreply.github.com>
2022-11-16 12:56:44 +0100

    Bug-Fix: Keep Bootstrap Tooltips correct after page-change

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -4,12 +4,12 @@
       <p>© 2022 Xcreen</p>
       <ul class="list-unstyled d-flex">
         <li class="ms-3">
-          <a class="link-dark" href="https://github.com/Xcreen" target="_blank" data-bs-toggle="tooltip" data-bs-placement="top" title="Xcreen on Github">
+          <a class="link-dark" href="https://github.com/Xcreen" target="_blank" ref="footerGithubLink" data-bs-toggle="tooltip" data-bs-placement="top" title="Xcreen on Github">
             <i class="fa-brands fa-github fa-xl"></i>
           </a>
         </li>
         <li class="ms-3">
-          <a class="link-dark" href="https://twitter.com/xWaschix" target="_blank" data-bs-toggle="tooltip" data-bs-placement="top" title="Xcreen on Twitter">
+          <a class="link-dark" href="https://twitter.com/xWaschix" target="_blank" ref="footerTwitterLink" data-bs-toggle="tooltip" data-bs-placement="top" title="Xcreen on Twitter">
             <i class="fa-brands fa-twitter fa-xl"></i>
           </a>
         </li>
@@ -26,10 +26,8 @@ export default {
   },
   mounted() {
     //Init Bootstrap-Tooltips
-    let tooltipTriggerList = [].slice.call(document.querySelectorAll('footer [data-bs-toggle="tooltip"]'))
-    tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
+    new bootstrap.Tooltip(this.$refs.footerGithubLink);
+    new bootstrap.Tooltip(this.$refs.footerTwitterLink);
   }
 }
 </script>
